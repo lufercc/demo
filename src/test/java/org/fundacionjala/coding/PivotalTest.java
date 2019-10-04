45f568f847d15522afddc654d5fe6b48d4ce84d2
Carlos Gonzales <carlos.gonzales@jalasoft.com>
2019-10-04 17:41:13 -0400

    adding more tests

--- src/test/java/org/fundacionjala/coding/PivotalTest.java
+++ src/test/java/org/fundacionjala/coding/PivotalTest.java
@@ -3,22 +3,41 @@ package org.fundacionjala.coding;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PivotalTest {
 
 
     @Test
-    public void testPOSTProject() {
-
+    public void testPUTProject() {
+        //Given
+        String expectedProjectName = "Rest Assured new";
         Response response = RestAssured.given()
                 .baseUri("https://www.pivotaltracker.com/services/v5")
                 .header("X-TrackerToken", "19d1e5ad024d2f6ed8f5e9e7e1c6f26a")
                 .contentType(ContentType.JSON)
                 .when()
-                .body("{\"name\":\"Project created by Rest Assured\"}")
+                .body("{\"name\":\"" + expectedProjectName + "\"}")
                 .post("/projects");
-        System.out.println(response.prettyPrint());
+
+        String actualProjectName = response.jsonPath().getString("name");
+        Assert.assertEquals(actualProjectName, expectedProjectName);
+
+        //When
+        String projectId = response.jsonPath().getString("id");
+        String expectedNewProjectName = "Test PUT";
+        response = RestAssured.given()
+                .baseUri("https://www.pivotaltracker.com/services/v5")
+                .header("X-TrackerToken", "19d1e5ad024d2f6ed8f5e9e7e1c6f26a")
+                .contentType(ContentType.JSON)
+                .when()
+                .body("{\"name\":\"" + expectedNewProjectName + "\"}")
+                .put("/projects/" + projectId);
+
+        //Then
+        actualProjectName = response.jsonPath().getString("name");
+        Assert.assertEquals(actualProjectName, expectedNewProjectName);
     }
 
 }
