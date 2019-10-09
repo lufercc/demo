a4e267c224fd4e2afd4534911b41ffa7cc1fb51a
Magali Fuentes <magali.fa@gmail.com>
2019-10-09 11:21:01 -0400

    practice with API methods

--- src/test/java/org/fundacionjala/coding/StoryTest.java
+++ src/test/java/org/fundacionjala/coding/StoryTest.java
@@ -13,29 +13,73 @@ public class StoryTest {
 
         //Given
         String expectedProjectName = "Rest Assured new1";
-        Response response = RestAssured.given(RequestSpecFactory.getRequestSpec("pivotal"))
-                .contentType(ContentType.JSON)
-                .when()
-                .body("{\"name\":\"" + expectedProjectName + "\"}")
-                .post("/projects");
+
+        Response response = RequestManager.post(RequestSpecFactory.getRequestSpec("pivotal"), "/projects",
+                "{\"name\":\"" + expectedProjectName + "\"}");
+
         String projectId = response.jsonPath().getString("id");
 
         String storyName = "My Story";
-        response = RestAssured.given(RequestSpecFactory.getRequestSpec("pivotal"))
-                .contentType(ContentType.JSON)
-                .when()
-                .body("{\"name\":\"" + storyName + "\"}")
-                .post(String.format("/projects/%s/stories", projectId));
+
+        response = RequestManager.post(RequestSpecFactory.getRequestSpec("pivotal"), String.format("/projects/%s/stories", projectId),
+                "{\"name\":\"" + storyName + "\"}");
+
         String storyId = response.jsonPath().getString("id");
 
         // When
-        String expectedNewStoryName= "New Story Name";
-        response = RestAssured.given(RequestSpecFactory.getRequestSpec("pivotal"))
-                .contentType(ContentType.JSON)
-                .when()
-                .body("{\"name\":\"" + expectedNewStoryName + "\"}")
-                .put(String.format("/projects/%s/stories/%s", projectId, storyId));
+        String expectedNewStoryName = "New Story Name";
+
+        response = RequestManager.put(RequestSpecFactory.getRequestSpec("pivotal"), String.format("/projects/%s/stories/%s", projectId, storyId),
+                "{\"name\":\"" + expectedNewStoryName + "\"}");
+
 
         Assert.assertEquals(response.jsonPath().getString("name"), expectedNewStoryName);
     }
+
+    @Test
+    public void testDeleteStory() {
+
+        //Given
+        String expectedProjectName = "Rest Assured delete";
+        Response response = RequestManager.post(RequestSpecFactory.getRequestSpec("pivotal"), "/projects",
+                "{\"name\":\"" + expectedProjectName + "\"}");
+        String projectId = response.jsonPath().getString("id");
+
+        String storyName = "My Story";
+        response = RequestManager.post(RequestSpecFactory.getRequestSpec("pivotal"), String.format("/projects/%s/stories", projectId),
+                "{\"name\":\"" + storyName + "\"}");
+        String storyId = response.jsonPath().getString("id");
+
+        // When
+        int expectedStatus = 204;
+        response = RequestManager.delete(RequestSpecFactory.getRequestSpec("pivotal"), String.format("/projects/%s/stories/%s", projectId, storyId));
+        //Then
+        int actualStatus = response.statusCode();
+        Assert.assertEquals(actualStatus, expectedStatus);
+    }
+
+    @Test
+    public void testGetStory() {
+
+        //Given
+        String expectedProjectName = "Rest Assured get";
+        Response response = RequestManager.post(RequestSpecFactory.getRequestSpec("pivotal"), "/projects",
+                "{\"name\":\"" + expectedProjectName + "\"}");
+        String projectId = response.jsonPath().getString("id");
+
+        String storyName = "My Story";
+        response = RequestManager.post(RequestSpecFactory.getRequestSpec("pivotal"), String.format("/projects/%s/stories", projectId),
+                "{\"name\":\"" + storyName + "\"}");
+        String storyId = response.jsonPath().getString("id");
+
+        // When
+        response = RequestManager.get(RequestSpecFactory.getRequestSpec("pivotal"), String.format("/projects/%s/stories/%s", projectId, storyId));
+        //Then
+
+        String actualStoryName = response.jsonPath().getString("name");
+        String actualProjectId = response.jsonPath().getString("project_id");
+        Assert.assertEquals(actualStoryName, storyName);
+        Assert.assertEquals(actualProjectId, projectId);
+    }
+
 }
