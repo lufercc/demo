9b850a47ae43999709b75cb5afaf1a85ae44c546
Luis Cachi <Luis Cachi@jalasoft.local>
2019-10-14 18:11:00 -0400

    - Adding POST, GET, DELETE, PUT Tc for Comment

---
+++ src/test/java/org/fundacionjala/coding/POSTCommentTest.java
@@ -0,0 +1,53 @@
+package org.fundacionjala.coding;
+
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class POSTCommentTest {
+    private String projectId;
+    private String storyId;
+    private String commentId;
+    String textComment = "this is comment text";
+    @BeforeTest
+    public void setUp() {
+        String expectedProjectName = "Rest Assured new";
+        String expectedStoryName = "Rest new Story 1";
+
+        Response response = RequestManager.post(RequestSpecFactory.getRequestSpec("pivotal"),
+                "/projects",
+                "{\"name\":\"" + expectedProjectName + "\"}");
+        projectId = response.jsonPath().getString("id");
+
+        Response resStory = RequestManager.post(RequestSpecFactory.getRequestSpec("pivotal"),
+                String.format("/projects/%s/stories", projectId),
+                "{\"name\":\""+expectedStoryName+"\"}");
+        storyId = resStory.jsonPath().getString("id");
+
+        Response comment = RequestManager.post(RequestSpecFactory.getRequestSpec("pivotal"),
+                String.format("/projects/%s/stories/%s/comments", projectId, storyId),
+                "{\"text\":\""+textComment+"\"}");
+        commentId = comment.jsonPath().getString("id");
+    }
+    @Test
+    public void testPostComment(){
+        Response comment = RequestManager.post(RequestSpecFactory.getRequestSpec("pivotal"),
+                String.format("/projects/%s/stories/%s/comments", projectId, storyId),
+                "{\"text\":\""+textComment+"\"}");
+        commentId = comment.jsonPath().getString("id");
+        Assert.assertEquals(comment.getStatusCode(), 200);
+        Response response = RequestManager.get(RequestSpecFactory.getRequestSpec("pivotal"),
+                String.format("/projects/%s/stories/%s/comments/%s", projectId, storyId, commentId));
+        String actualProjectName = response.jsonPath().getString("text");
+        Assert.assertEquals(actualProjectName, textComment);
+        Assert.assertEquals(comment.getStatusCode(), 200);
+    }
+
+    @AfterTest
+    public void cleanData() {
+        RequestManager.delete(RequestSpecFactory.getRequestSpec("pivotal"),
+                String.format("/projects/%s", projectId));
+    }
+}
